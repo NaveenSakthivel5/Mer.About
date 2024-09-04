ed38ac06becc2ebf6885da1ffe8b7a01ce4dea04
NaveenSakthivel5 <naveensakthivel14@gmail.com>
2024-09-04 15:18:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import About from './pages/About';
+
+
+
+
+export default function App() {
+  return (
+    <About />
+  );
+}
---
+++ src/pages/About.jsx
@@ -0,0 +1,282 @@
+import './About.css'
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="container-2">
+        <div className="container-6">
+          <img className="layer-1" src="assets/vectors/Layer1_x2.svg" />
+          <div className="about-contact">
+            about  -  contact
+          </div>
+        </div>
+        <div className="group-21">
+          <div className="rectangle-1577">
+          </div>
+          <div className="container-1">
+            <div className="rectangle-1573">
+            </div>
+            <div className="rectangle-1576">
+            </div>
+          </div>
+          <div className="container-10">
+            <div className="rectangle-1572">
+            </div>
+            <div className="rectangle-1575">
+            </div>
+          </div>
+          <div className="container-11">
+            <div className="rectangle-1571">
+            </div>
+            <div className="rectangle-1574">
+            </div>
+          </div>
+          <div className="rectangle-1570">
+          </div>
+          <div className="rectangle-1568">
+          </div>
+          <div className="container-9">
+            <div className="rectangle-1565">
+            </div>
+            <div className="rectangle-1566">
+            </div>
+          </div>
+          <div className="container-8">
+            <div className="rectangle-1567">
+            </div>
+            <div className="rectangle-1569">
+            </div>
+          </div>
+          <div className="rectangle-1564">
+          </div>
+          <div className="container-4">
+            <div className="rectangle-1562">
+            </div>
+            <div className="rectangle-1561">
+            </div>
+          </div>
+          <div className="container-3">
+            <div className="rectangle-1563">
+            </div>
+            <div className="rectangle-1560">
+            </div>
+          </div>
+          <div className="rectangle-1559">
+          </div>
+          <div className="container-7">
+            <div className="rectangle-1557">
+            </div>
+            <div className="rectangle-1558">
+            </div>
+            <div className="rectangle-1552">
+            </div>
+          </div>
+          <div className="container-5">
+            <div className="rectangle-1555">
+            </div>
+            <div className="rectangle-1553">
+            </div>
+          </div>
+          <div className="container">
+            <div className="rectangle-1556">
+            </div>
+            <div className="rectangle-1554">
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="image">
+          </div>
+          <span className="electrifying">
+            Electrifying
+          </span>
+          <span className="the-thrill">
+            THE Thrill
+          </span>
+          <span className="of-riding">
+            OF Riding
+          </span>
+          <span className="transforming-mobility-one-exhilarating-ride-at-atime">
+            Transforming Mobility,One<br />Exhilarating Ride at a<br />Time
+          </span>
+        </div>
+        <div className="group-18">
+          <div className="rectangle-1550">
+          </div>
+          <div className="rectangle-1549">
+          </div>
+          <div className="rectangle-1548">
+          </div>
+        </div>
+        <div className="group-49">
+          <p className="container-1">
+            /02 WHO WE ARE
+          </p>
+        </div>
+
+        <div className="innovators">
+          <span>Innovators<br /> in Motion</span>
+        </div>
+
+        <span className="at-mercellenie">
+          At-mercellenie-automotive-we-epitomize-excellence-in-electric-mobility-we-craft-premium-electric-bikes-that-set-new-benchmarks-for-performance-and-environmental-responsibility-we-deliver-exceptional-quality-and-innovation-in-electric-mobility-setting-new-global-standards-with-premium-aesthetics-unmatched-performance-extensive-range-and-fast-charging-sub-7
+        </span>
+        <div className="group-48">
+          <p className="container">
+            /02 OUR APPROACH
+          </p>
+        </div>
+        <span className="we-believe">
+          we believe in delivering electric vehicles that are not just eco-friendly but also luxurious and high-performing. We focus on creating a driving experience that is unmatched, where the power of electric meets the luxury of high-end cars. Our team is dedicated to pushing the boundaries of technology to build the best electric vehicles.
+        </span>
+
+        <div className="power-style-efficiency">
+          <span className="power">POWER</span>
+          <span className="separator"></span>
+          <span className="style">STYLE</span>
+          <span className="separator bg-color"></span>
+          <span className="efficiency">EFFICIENCY</span>
+        </div>
+
+        <div className="passion-meets-innovation">
+          <div className="screenshot-image">
+            <img src="assets/images/Screenshot.png" alt="Sketching e-bike design" />
+          </div>
+          <div className="innovation-text">
+            <h1>PASSION<br />MEETS<br />INNOVATION</h1>
+            <p>
+              Discover the new Roetz Life e-bike in our Fair Factory in Amsterdam. The sneak peek includes a closer look at the 3D-printed e-bike model and a test ride on one of our latest prototypes.
+            </p>
+          </div>
+        </div>
+
+        <div className="why-choose-us">
+          <div className="why-text">
+            <p className="section-number">/03 WHY CHOOSE US</p>
+            <h2>INNOVATIVE ELECTRIC BIKES FOR A BETTER TOMORROW!</h2>
+          </div>
+          <div className="why-icons">
+            <div className="icon-item">
+              <div className="frame-49">
+                <img className="vector" src="src/assets/vectors/Vector11.svg" alt="Icon" />
+              </div>
+              <div className="icon-text">
+                <h3>SUSTAINABLE FUTURE</h3>
+                <p>Creating eco-friendly bikes for a better tomorrow.</p>
+              </div>
+            </div>
+            <div className="icon-item">
+              <div className="frame-49">
+                <img className="vector" src="src/assets/vectors/Vector12.svg" alt="Icon" />
+              </div>
+              <div className="icon-text">
+                <h3>HIGH PERFORMANCE</h3>
+                <p>Experience superior speed and efficiency.</p>
+              </div>
+            </div>
+            <div className="icon-item">
+              <div className="frame-49">
+                <img className="vector" src="src/assets/vectors/Vector13.svg" alt="Icon" />
+              </div>
+              <div className="icon-text">
+                <h3>GLOBAL IMPACT</h3>
+                <p>Proudly serving customers worldwide, from India.</p>
+              </div>
+            </div>
+            <div className="icon-item">
+              <div className="frame-49">
+                <img className="vector" src="src/assets/vectors/Vector14.svg" alt="Icon" />
+              </div>
+              <div className="icon-text">
+                <h3>PIONEERING INNOVATION</h3>
+                <p>Leading advancements in electric bike technology.</p>
+              </div>
+            </div>
+            <div className="icon-item">
+              <div className="frame-49">
+                <img className="vector" src="src/assets/vectors/Vector15.svg" alt="Icon" />
+              </div>
+              <div className="icon-text icon-text-growing-demand">
+                <h3>GROWING DEMAND</h3>
+                <p>Positioned to cap  ture the expanding market for electric bikes with our innovative and high-performing products.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="image-text-container">
+          <img src="assets/images/Mercelline.png" alt="Mercelline" className="mercelline-image" />
+          <div className="overlay-text">
+            <p>
+              “EMBRACE THE THRILL GUILT-FREE WITH THE ELECTRIC SUPERBIKE. <br />
+              EXPERIENCE <span className="highlight">POWERFUL SPEED AND ECO-FRIENDLY</span> DESIGN. CONQUER THE ROAD AND REDUCE YOUR CARBON FOOTPRINT WITH MERCELLENIE”
+            </p>
+          </div>
+        </div>
+
+
+        <div class="milestones-section">
+          <div class="section-title">
+            <span class="milestone-number">/04 MileStones</span>
+            <h2>A Journey of Innovation and Perseverance</h2>
+          </div>
+          <div class="milestones">
+            <div class="milestone">
+              <div class="year">/2024</div>
+              <div class="milestone-text">
+                <h3>Ready for Launch</h3>
+                <p>By February 2024, our pre-production bike passed rigorous testing. Teaser shoots wrapped up by May, and now Mercelleinie is ready to launch. From sketches to a superbike, our journey has been driven by innovation and perseverance. Every setback taught us resilience, and today, Mercelleinie stands ready to revolutionize electric mobility.</p>
+              </div>
+            </div>
+            <div class="milestone">
+              <div class="year">/2023</div>
+              <div class="milestone-text">
+                <h3>Major Developments</h3>
+                <p>In 2023, Mercelleinie focused on a pre-production superbike with a custom high-capacity battery and new chassis. After over 63 design iterations, we created a compact, high-energy battery pack. Despite setbacks like overheating, we refined the design, leading to successful tests and the start of our teaser video production.</p>
+              </div>
+            </div>
+            <div class="milestone">
+              <div class="year">/2022</div>
+              <div class="milestone-text">
+                <h3>Overcoming Challenges</h3>
+                <p>In 2022, I developed a new motor with a vendor and improved the prototype by removing the bulky CVT system. Reuniting with my former teammate, Sanjidth, we focused on a direct drive motor, reducing weight and enhancing performance. This year was crucial for overcoming obstacles and refining the design.</p>
+              </div>
+            </div>
+            <div class="milestone">
+              <div class="year">/2021</div>
+              <div class="milestone-text">
+                <h3>Gaining Momentum</h3>
+                <p>During the 2021 lockdown, I pitched my electric motorcycle concept to my father. He supported me, allowing me to build my first prototype. Despite challenges with battery tech and components, this phase laid the groundwork for future success.</p>
+              </div>
+            </div>
+            <div class="milestone milestone-special">
+              <div class="year">/2018 - 2020</div>
+              <div class="milestone-text">
+                <h3>Building Foundations</h3>
+                <p>In college, I joined the Baja team, learning vehicle dynamics and manufacturing while leading our team in competitions. The 2020 lockdown reignited my passion, inspiring me to focus on electric bikes.</p>
+              </div>
+            </div>
+
+
+            <div class="milestone">
+              <div class="year">/2017</div>
+              <div class="milestone-text">
+                <h3>The Spark of Inspiration</h3>
+                <p>By February 2024, our pre-production bike passed rigorous testing. Teaser shoots wrapped up by May, and now Mercelleinie is ready to launch. From sketches to a superbike, our journey has been driven by innovation and perseverance. Every setback taught us resilience, and today, Mercelleinie stands ready to revolutionize electric mobility.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+      </div>
+    </div>
+  )
+}
+
+
